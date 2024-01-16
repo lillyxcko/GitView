402c8c09fcada92fd71ba89c1269867f2d1dc531
Lilly Ko <lillyxcko@gmail.com>
2024-01-15 20:25:40 -0800

    logo fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="container">
         <img 
-            src={'./public/gitview.png'}
+            src={'/gitview.png'}
             alt="GitHub View"
             className="logo--image"/>
         <h2 className="logo--text">GitView</h2>
